ed148ca03bda7a0346d995399ce47795f6101a1c
GURUGUBELLI SAI KUMAR <58288084+sai693@users.noreply.github.com>
2024-12-18 01:40:16 +0000

    S1

--- python-for-devops/Day-02/examples/03-regex-match.py
+++ python-for-devops/Day-02/examples/03-regex-match.py
@@ -1,7 +1,7 @@
 import re
 
 text = "The quick brown fox"
-pattern = r"quick"
+pattern = r"quick brown fox"
 
 match = re.match(pattern, text)
 if match:
--- python-for-devops/Day-02/test.py
+++ python-for-devops/Day-02/test.py
@@ -1,2 +1,15 @@
+import re
 arn = "arn:aws:iam::12345678912:user/saikumar"
-print (arn.split("/"))
+print (arn.split("/")[1])
+
+a = (arn.split("/")[1])
+print (a.upper())
+b = "Gurugubelli"
+print (a.upper() + " " + b.upper())
+text = "its match the quick pattern"
+pattern = r"quick"
+match = re.match(pattern, text)
+if match:
+    print("match found", match.group())
+else:
+    print("Not Found")
