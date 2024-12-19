08ba3812a5d64d8e145b9b4d89e42b9de27b6a92
GURUGUBELLI SAI KUMAR <58288084+sai693@users.noreply.github.com>
2024-12-19 00:21:44 +0000

    10

--- python-for-devops/Day-10/inp.py
+++ python-for-devops/Day-10/inp.py
@@ -1,7 +1,15 @@
 import os
 
-fold = input("please give me you folder input").split()
+folders = input("please give me you folder input").split()
 
-for folder in fold:
-    files = os.listdir(folder)
-    print(files)
+for folder in folders:
+    try:
+        files = os.listdir(folder)
+    except FileNotFoundError:
+        print("Please provide a valid folder name")
+        break
+
+    print("=== Listings file from the folders " +folder)
+
+    for file in files:
+        print(file)
